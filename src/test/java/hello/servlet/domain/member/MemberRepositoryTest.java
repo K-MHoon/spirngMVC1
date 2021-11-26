23e0baccda17f2b9c00fe71032e194cb230ca08e
K-MHoon <azmria117@gmail.com>
2021-11-26 10:16:07 +0900

    feat : JSP 활용 회원 추가, 조회, 입력 기능 구현

---
+++ src/test/java/hello/servlet/domain/member/MemberRepositoryTest.java
@@ -0,0 +1,47 @@
+package hello.servlet.domain.member;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberRepositoryTest {
+
+    MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @AfterEach
+    void afterEach() {
+        memberRepository.clearStore();
+    }
+
+    @Test
+    void save() {
+        //given
+        Member hello = new Member("hello", 20);
+
+        //when
+        Member savedMember = memberRepository.save(hello);
+
+        //then
+        Member findMember = memberRepository.findById(savedMember.getId());
+        assertEquals(findMember, savedMember);
+    }
+
+    @Test
+    void findAll() {
+        Member member1 = new Member("member1", 20);
+        Member member2 = new Member("member2", 30);
+
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        List<Member> result = memberRepository.findAll();
+
+        assertEquals(result.size(), 2);
+        org.assertj.core.api.Assertions.assertThat(result).contains(member1, member2);
+    }
+
+}
