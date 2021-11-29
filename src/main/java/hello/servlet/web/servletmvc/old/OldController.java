39a8f2a427a08d2f22033f78620b89cda7ceb2ae
K-MHoon <azmria117@gmail.com>
2021-11-29 13:30:01 +0900

    feat : 유연한 컨트롤러2 (version 5)
    - OldVersion Spring Controller 테스트
    - Handler 우선순위에 따른 결괏값 테스트

---
+++ src/main/java/hello/servlet/web/servletmvc/old/OldController.java
@@ -0,0 +1,22 @@
+package hello.servlet.web.servletmvc.old;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 고전 Spring Controller의 형태
+ */
+@Component("/springmvc/old-controller")
+public class OldController implements Controller {
+
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        System.out.println("OldController.handleRequest");
+        return new ModelAndView("new-form");
+    }
+}
+
